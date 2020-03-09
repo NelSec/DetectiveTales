a73250f999e8ee9945b7635be62a96c1050c7772
Gustavo Sobral <gugadramon2@gmail.com>
2020-03-09 14:21:29 +0000

    camera movement and panels

---
+++ New Unity Project (1)/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] goalObject = null;
+
+    [SerializeField]
+    private Transform goal = null;
+
+    private int currentGoalObject = 0;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            goal = goalObject[currentGoalObject].transform;
+            
+            currentGoalObject++;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, goal.position, step);
+    }
+}
---
+++ New Unity Project (1)/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(1f, 1f, 1f);
+    }
+}
