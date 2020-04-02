1b80240ba2711dd0cafea84f12a170ee589ba8cf
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2020-04-02 17:23:28 +0100

    Add panel occlusion

--- DetectiveTales/Assets/Scripts/ExtCameraMovement.cs
+++ DetectiveTales/Assets/Scripts/ExtCameraMovement.cs
@@ -11,7 +11,12 @@ public class ExtCameraMovement : MonoBehaviour
     [SerializeField]
     private Transform goal = null;
 
-    private int currentGoalObject = 0;
+    public Animator animator;
+    public Animator animator2;
+    public Animator animator3;
+    public Animator animator4;
+
+    public int currentGoalObject = 0;
     public float speed = 1.0f;
 
     void Update()
@@ -19,11 +24,26 @@ public class ExtCameraMovement : MonoBehaviour
         float step = speed * Time.deltaTime;
         if (Input.GetMouseButtonDown(0))
         {
-            
+            switch (currentGoalObject)
+            {
+                case (1):
+                    animator.SetTrigger("FadeOut");
+                    break;
+                case (2):
+                    animator2.SetTrigger("FadeOut2");
+                    break;
+                case (3):
+                    animator3.SetTrigger("FadeOut3");
+                    break;
+                case (4):
+                    animator4.SetTrigger("FadeOut4");
+                    break;
+            }
             goal = goalObject[currentGoalObject].transform;
             
             currentGoalObject++;
         }
-        transform.position = Vector3.MoveTowards(transform.position, goal.position, step);
+        transform.position = Vector3.MoveTowards(
+            transform.position, goal.position, step);
     }
 }
