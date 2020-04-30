7f27f7c84eb930657c4b637a45cd33b358388f8e
Gustavo Sobral <gugadramon2@gmail.com>
2020-04-30 09:19:28 +0100

    shooting player - basic mechanics

---
+++ DetectiveTales/Assets/Scripts/RayCast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public Transform targetTransform;
+    Vector3 direction;
+
+    private float rayDistance;
+
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayDistance = 100f;
+        direction = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = targetTransform.position - transform.position;
+
+        Debug.DrawRay(transform.position, direction * rayDistance, Color.magenta);
+
+        if (Physics.Raycast(transform.position, direction * rayDistance, out hit, rayDistance) && hit.transform.tag == "Enemy" && Input.GetKeyDown("space"))
+        {
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
---
+++ DetectiveTales/Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float hValue;
+    private float vValue;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hValue = Input.GetAxis("Horizontal");
+        vValue = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(hValue,vValue,0f);
+
+        rb.velocity = direction * 200f * Time.deltaTime;
+    }
+}
