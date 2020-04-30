1a8e63c215c92f5ffb8011b041ac2c2d0b829961
Gustavo Sobral <gugadramon2@gmail.com>
2020-04-30 14:05:22 +0100

    Revert "deleted a directory that I added by mistake"
    This reverts commit 434fd9b2b71476b623637a98b423a93d030e2996.

---
+++ Erased/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 move;
+    private float speed = 100f;
+    private float xMove;
+    private float yMove;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        move = Vector2.zero;
+    }
+
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal");
+        yMove = Input.GetAxis("Vertical");
+
+        move = new Vector2(xMove, yMove);
+
+        rb.AddForce(move * speed * Time.deltaTime);
+    }
+}
