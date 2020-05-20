a7deaf3fd108813edfa501bc69775ac86c92d538
Gustavo Sobral <gugadramon2@gmail.com>
2020-05-20 13:02:56 +0100

    transition scripts start

---
+++ DetectiveTales/Assets/Scripts/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController _instance;
+
+    public static SceneController instance
+    {
+        get { return _instance; }
+    }
+
+    public bool objetiveDone;
+
+    [SerializeField]
+    private GameObject[] scenes;
+
+    void Start()
+    {
+        _instance = this;
+        objetiveDone = false;
+    }
+
+    void Update()
+    {
+        
+    }
+}
