809a56b8f03a338bb4cfe7f878898bf136db985c
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2020-09-12 16:55:32 +0100

    Switched to TMP, more customization for each panel text

--- DetectiveTales/Assets/Scripts/Player/Interaction/CanvasManager.cs
+++ DetectiveTales/Assets/Scripts/Player/Interaction/CanvasManager.cs
@@ -1,29 +1,57 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class CanvasManager : MonoBehaviour
 {
     public GameObject inspectionPanel;
+    public GameObject inspectionPanel2;
     public GameObject thoughtPanel;
-    public Text inspectionText;
+    public TextMeshProUGUI inspectionText;
+    public TextMeshProUGUI inspectionText2;
     public Text thoughtText;
     public Image[] inventoryIcons;
 
+    public int currentPanel = 0;
+
     public void Start()
     {
-        HideInteractionPanels();
+        //HideInteractionPanels();
+    }
+
+    public void Update()
+    {
+        if (SceneController.instance.objetiveDone == true)
+            currentPanel++;
     }
 
     public void ShowInteractionPanel(string inspectionMessage)
     {
         inspectionText.text = inspectionMessage;
-        inspectionPanel.SetActive(true);
+        inspectionText2.text = inspectionMessage;
+        switch (currentPanel)
+        {
+            case (1):
+                inspectionPanel.SetActive(true);
+                break;
+            case (2):
+                inspectionPanel2.SetActive(true);
+                break;
+        }       
     }
 
     public void HideInteractionPanels()
     {
-        inspectionPanel.SetActive(false);
         thoughtPanel.SetActive(false);
+        switch (currentPanel)
+        {
+            case (2):
+                inspectionPanel.SetActive(false);
+                break;
+            case (3):
+                inspectionPanel2.SetActive(false);
+                break;
+        }     
     }
 
     public void ClearInventoryIcons()
