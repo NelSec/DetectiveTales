b72a776fcf335b535f5f32e32f9054ce01ca54e6
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2020-09-14 17:41:09 +0100

    Adjusted text in each panel

--- DetectiveTales/Assets/Scripts/Player/Interaction/CanvasManager.cs
+++ DetectiveTales/Assets/Scripts/Player/Interaction/CanvasManager.cs
@@ -6,9 +6,29 @@ public class CanvasManager : MonoBehaviour
 {
     public GameObject inspectionPanel;
     public GameObject inspectionPanel2;
+    public GameObject inspectionPanel3;
+    public GameObject inspectionPanel4;
+    public GameObject inspectionPanel5;
+    public GameObject inspectionPanel6;
+    public GameObject inspectionPanel7;
+    public GameObject inspectionPanel8;
+    public GameObject inspectionPanel9;
+    public GameObject inspectionPanel10;
+    public GameObject inspectionPanel11;
+    public GameObject inspectionPanel12;
     public GameObject thoughtPanel;
     public TextMeshProUGUI inspectionText;
     public TextMeshProUGUI inspectionText2;
+    public TextMeshProUGUI inspectionText3;
+    public TextMeshProUGUI inspectionText4;
+    public TextMeshProUGUI inspectionText5;
+    public TextMeshProUGUI inspectionText6;
+    public TextMeshProUGUI inspectionText7;
+    public TextMeshProUGUI inspectionText8;
+    public TextMeshProUGUI inspectionText9;
+    public TextMeshProUGUI inspectionText10;
+    public TextMeshProUGUI inspectionText11;
+    public TextMeshProUGUI inspectionText12;
     public Text thoughtText;
     public Image[] inventoryIcons;
 
@@ -37,6 +57,36 @@ public class CanvasManager : MonoBehaviour
             case (2):
                 inspectionPanel2.SetActive(true);
                 break;
+            case (3):
+                inspectionPanel3.SetActive(true);
+                break;
+            case (4):
+                inspectionPanel4.SetActive(true);
+                break;
+            case (5):
+                inspectionPanel5.SetActive(true);
+                break;
+            case (6):
+                inspectionPanel6.SetActive(true);
+                break;
+            case (7):
+                inspectionPanel7.SetActive(true);
+                break;
+            case (8):
+                inspectionPanel8.SetActive(true);
+                break;
+            case (9):
+                inspectionPanel9.SetActive(true);
+                break;
+            case (10):
+                inspectionPanel10.SetActive(true);
+                break;
+            case (11):
+                inspectionPanel11.SetActive(true);
+                break;
+            case (12):
+                inspectionPanel12.SetActive(true);
+                break;
         }       
     }
 
@@ -50,6 +100,33 @@ public class CanvasManager : MonoBehaviour
                 break;
             case (3):
                 inspectionPanel2.SetActive(false);
+                break;
+            case (4):
+                inspectionPanel3.SetActive(false);
+                break;
+            case (5):
+                inspectionPanel4.SetActive(false);
+                break;
+            case (6):
+                inspectionPanel5.SetActive(false);
+                break;
+            case (7):
+                inspectionPanel6.SetActive(false);
+                break;
+            case (8):
+                inspectionPanel7.SetActive(false);
+                break;
+            case (9):
+                inspectionPanel8.SetActive(false);
+                break;
+            case (10):
+                inspectionPanel9.SetActive(false);
+                break;
+            case (11):
+                inspectionPanel10.SetActive(false);
+                break;
+            case (12):
+                inspectionPanel11.SetActive(false);
                 break;
         }     
     }
